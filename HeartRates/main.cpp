7f417c55a4a6b5c3722194da483a4b5f7d09753f
wired-cyberbird <wiredcyberbird@protonmail.com>
2020-11-13 21:32:04 -0500

    class HeartRates fix

--- HeartRates/main.cpp
+++ HeartRates/main.cpp
@@ -8,7 +8,7 @@ int main()
 
     cout << "First name: " << Patient1.getFirstName()
         << "\nLast name: " << Patient1.getLastName()
-        << "\nDate Of Birth: " << Patient1.getDay() << "/" << Patient1.getMonth() << "/" << Patient1.getYear();
+        << "\nDate Of Birth: " << Patient1.getDay() << "/" << Patient1.getMonth() << "/" << Patient1.getYear()
         << "\nAge: " << Patient1.getAge()
         << "\nMaximum Heart Rate: " << Patient1.getMaximumHeartRate()
         << "\nTarget Heart Rate: " << Patient1.getTargetHeartRate() << endl; 
