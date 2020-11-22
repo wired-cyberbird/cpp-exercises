e6a4583970ba80ccbbb6383b495a69882b84d80e
wired-cyberbird <wiredcyberbird@protonmail.com>
2020-11-22 18:31:13 -0500

    multiples of 2 infinite loop

---
+++ m2_infloop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int multiple = 0;
+    
+    while(true)
+    {
+        cout << 2 * multiple << endl;
+        multiple++;
+    }
+}
