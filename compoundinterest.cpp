4bb70def577974606fa028530b655ab478f29b7a
wired-cyberbird <wiredcyberbird@protonmail.com>
2020-12-03 20:44:37 -0500

    input deposit and number of years

--- compoundinterest.cpp
+++ compoundinterest.cpp
@@ -8,9 +8,12 @@ int main()
     double amount = 0;
     double principal = 0;
     double rate = 0.05;
+    unsigned int years = 0;
 
     cout << "Please insert initial deposit of investment: ";
     cin >> principal;
+    cout << "insert years to calculate compound interest: ";
+    cin >> years;
 
     cout << "Year" << setw(21) << "Amount on deposit" << endl;
 
@@ -20,11 +23,11 @@ int main()
     {
         cout << "compound interest at " << rate*100 << "%" << endl;
 
-        for(unsigned int year = 1; year <= 10; year++)
+        for(unsigned int year = 1; year <= years; year++)
         {
             amount += principal * pow(1.0 + rate, year);
 
-            cout << setw(4) << year << setw(21) << amount << endl;
+            cout << setw(4) << year << setw(27) << amount << endl;
         }
 
         rate = rate + 0.01;
